da1aab427f3bfdf07e483649ca4a124eeb3aa13b
André Jerónimo <andre.jeronimo.93@gmail.com>
2020-08-06 21:15:01 +0100

    Exercise 5 - Define a Method - complete

--- exercises/src/main/scala/Time.scala
+++ exercises/src/main/scala/Time.scala
@@ -3,4 +3,8 @@ class Time(val hours: Int, val minutes: Int) {
   // TODO: Verify that minutes is within 0 and 59
 
   val asMinutes: Int = hours * 60 + minutes
+
+  def minus(that: Time): Int = {
+    asMinutes - that.asMinutes
+  }
 }
