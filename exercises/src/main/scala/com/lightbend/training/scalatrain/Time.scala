c5f08512087cf86beaa37b7acdbcc77e5452c54d
André Jerónimo <andre.jeronimo.93@gmail.com>
2020-10-17 11:39:58 +0100

    Exercise 9 - Define a Companion Object

--- exercises/src/main/scala/com/lightbend/training/scalatrain/Time.scala
+++ exercises/src/main/scala/com/lightbend/training/scalatrain/Time.scala
@@ -1,5 +1,7 @@
 package com.lightbend.training.scalatrain
 
+import scala.math.Integral.Implicits._
+
 class Time(val hours: Int = 0, val minutes: Int = 0) {
   // TODO: Verify that hours is within 0 and 23
   // TODO: Verify that minutes is within 0 and 59
@@ -12,5 +14,13 @@ class Time(val hours: Int = 0, val minutes: Int = 0) {
 
   def -(that: Time): Int = {
     minus(that)
+  }
+}
+
+object Time {
+
+  def fromMinutes(totalMinutes: Int): Time = {
+    val (hours, minutes) = totalMinutes /% 60
+    new Time(hours, minutes)
   }
 }
