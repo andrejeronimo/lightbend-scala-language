d64d4d3e5d117c73e18ab0c36b3a9911445471a8
André Jerónimo <andre.jeronimo.93@gmail.com>
2020-10-20 20:21:50 +0100

    Exercise 13 - Use a Sequence

--- exercises/src/main/scala/com/lightbend/training/scalatrain/Train.scala
+++ exercises/src/main/scala/com/lightbend/training/scalatrain/Train.scala
@@ -1,3 +1,9 @@
 package com.lightbend.training.scalatrain
 
-case class Train(kind: String, number: Int)
+case class Station(name: String)
+
+case class Train(kind: String, number: Int, schedule: Seq[Station]) {
+
+  require(schedule.size >= 2, "Schedule must contains at least 2 elements")
+
+}
